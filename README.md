225f29c06255bdc1d309a902fb541e7d267845d3
Pi <paingthetko.pi@gmail.com>
2023-02-23 20:14:55 -0500

    Init Done

--- README.md
+++
@@ -1,2 +0,0 @@
-# orbital-simulation
- Project Files for Computer Simulation Class Project 1
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-NODE_ENV=production
-
-npm run site-build
-
-pm2-runtime start ./node_modules/.bin/next -- start --port 17000 --name arwes
